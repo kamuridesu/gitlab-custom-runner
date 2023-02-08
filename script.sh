443507e997f4a81fc144f99d353c6e85ad0d6682
Kamuri Amorim <myk.gata14@gmail.com>
2023-02-08 12:09:28 -0300

    fix: fixing dns hosts function to add aliases to the end of the file
    this avoid the configuraton beign read inside the array

--- script.sh
+++ script.sh
@@ -7,48 +7,30 @@ addHostsToConfig() {
         exit 1
     fi
 
-    LAST_IP_ENTRY=""
-    HOSTNAMES=""
-
     filename="/etc/gitlab-runner/config.toml"
-    IFS=''
-    NEW_TEXT=""
+    NEW_TEXT="$(cat $filename)"
     NEWLINE=$'\n'
-    while read line; do
-        if [[ "$NEW_TEXT" != "" ]]; then
-            NEW_TEXT="${NEW_TEXT}${NEWLINE}"
-        fi
-        NEW_TEXT="${NEW_TEXT}${line}"
-        if [[ "$line" == *"[runners.kubernetes]"* ]]; then
-            HOST_ALIAS_HEADER="    [[runners.kubernetes.host_aliases]]"
-            NEW_TEXT="${NEW_TEXT}${NEWLINE}${HOST_ALIAS_HEADER}"
-            IFS=";"
-            for DNS_IP in $DNS_IP_LIST; do
-                LAST_IP_ENTRY="$(echo -n $DNS_IP | cut -d ':' -f 1)"
-                HOSTS="$(echo -n $DNS_IP | cut -d ':' -f 2)"
-                STRING="\"ip\"=$IP"
-                IFS=' '
-                LAST_HOST="$(echo ${HOSTS##* })"
-                HOSTNAMES="["
-                for DNS in $HOSTS; do
-                    HOSTNAMES="${HOSTNAMES}\"${DNS}\""
-                    if [[ "$DNS" != "$LAST_HOST" ]]; then
-                        HOSTNAMES="${HOSTNAMES}, "
-                    fi
-                done
-                HOSTNAMES="${HOSTNAMES}]"
-                IFS=";"
-                NEW_TEXT="${NEW_TEXT}${NEWLINE}      ip = \"${LAST_IP_ENTRY}\"${NEWLINE}      hostnames = ${HOSTNAMES}"
-            done
-            IFS=''
-        fi
-    done < $filename
-    NEW_TEXT="${NEW_TEXT}${NEWLINE}${line}"
-
+    HOST_ALIAS_HEADER="    [[runners.kubernetes.host_aliases]]"
+    IFS=";"
+    for DNS_IP in $DNS_IP_LIST; do
+        LAST_IP_ENTRY="$(echo -n $DNS_IP | cut -d ':' -f 1)"
+        HOSTS="$(echo -n $DNS_IP | cut -d ':' -f 2)"
+        IFS=' '
+        LAST_HOST="$(echo ${HOSTS##* })"
+        NEW_TEXT="${NEW_TEXT}${NEWLINE}${HOST_ALIAS_HEADER}"
+        HOSTNAMES="["
+        for DNS in $HOSTS; do
+            HOSTNAMES="${HOSTNAMES}\"${DNS}\""
+            if [[ "$DNS" != "$LAST_HOST" ]]; then
+                HOSTNAMES="${HOSTNAMES}, "
+            fi
+        done
+        HOSTNAMES="${HOSTNAMES}]"
+        IFS=";"
+        NEW_TEXT="${NEW_TEXT}${NEWLINE}      ip = \"${LAST_IP_ENTRY}\"${NEWLINE}      hostnames = ${HOSTNAMES}"
+    done
     echo $NEW_TEXT > /etc/gitlab-runner/config.toml
-
 }
-
 
 RETRIES=0
 while [[ "$(curl --connect-timeout 15 --max-time 30 -Ls -o /dev/null -w ''%{http_code}'' ${GITLAB_HOST})" != "200" ]]; do
@@ -64,10 +46,10 @@ gitlab-runner register --non-interactive --url="${GITLAB_HOST}" --registration-t
 
 if [ "${EXECUTOR}" == "kubernetes" ]; then
     sed -i "s/namespace = \"\"/namespace = \"${NAMESPACE}\"/" /etc/gitlab-runner/config.toml
+    if [[ "$DNS_IP_LIST" != "" ]]; then
+        addHostsToConfig "$DNS_IP_LIST"
+    fi
 fi
 
-if [[ "$DNS_IP_LIST" != "" ]]; then
-    addHostsToConfig "$DNS_IP_LIST"
-fi
 
 /usr/bin/dumb-init /entrypoint run --user=gitlab-runner --working-directory=/home/gitlab-runner
