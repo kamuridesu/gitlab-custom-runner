404acf3428a88b59e412fe0bc213da9574114e50
Kamuri Amorim <myk.gata14@gmail.com>
2023-02-09 17:50:31 -0300

    feature: adding annotations to pod

--- script.sh
+++ script.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+checkGitlabConnection() {
+    RETRIES=0
+    while [[ "$(curl --connect-timeout 15 --max-time 30 -Ls -o /dev/null -w ''%{http_code}'' ${GITLAB_HOST})" != "200" ]]; do
+        echo "[*] Checking connection with ${GITLAB_HOST}"
+        ((RETRIES=RETRIES+=1))
+        if [[ $RETRIES -eq 3 ]]; then
+            echo "Could not connect to ${GITLAB_HOST}!"
+            exit 1
+        fi
+        sleep 5
+    done
+}
+
 addHostsToConfig() {
     DNS_IP_LIST=$1
     if [[ "$DNS_IP_LIST" == "" ]]; then
-        echo "[!] Error! 'DNS:IP;' expected!"
+        echo "[!] Error! 'IP:DNS;' expected!"
         exit 1
     fi
 
@@ -32,24 +45,53 @@ addHostsToConfig() {
     echo $NEW_TEXT > /etc/gitlab-runner/config.toml
 }
 
-RETRIES=0
-while [[ "$(curl --connect-timeout 15 --max-time 30 -Ls -o /dev/null -w ''%{http_code}'' ${GITLAB_HOST})" != "200" ]]; do
-    echo "[*] Checking connection with ${GITLAB_HOST}"
-    ((RETRIES=RETRIES+=1))
-    if [[ $RETRIES -eq 3 ]]; then
-        echo "Could not connect to ${GITLAB_HOST}!"
+addAnnotationsToPod() {
+    ANNOTATIONS=$1
+    if [[ "$ANNOTATIONS" == "" ]]; then
+        echo "[!] Error! 'key=value' expected!"
         exit 1
     fi
-    sleep 5
-done
-gitlab-runner register --non-interactive --url="${GITLAB_HOST}" --registration-token="${REGISTRATION_TOKEN}" --executor="${EXECUTOR}" --description="${DESCRIPTION}" --tag-list="${TAG}"
+    oIFS="$IFS"
 
-if [ "${EXECUTOR}" == "kubernetes" ]; then
-    sed -i "s/namespace = \"\"/namespace = \"${NAMESPACE}\"/" /etc/gitlab-runner/config.toml
-    if [[ "$DNS_IP_LIST" != "" ]]; then
-        addHostsToConfig "$DNS_IP_LIST"
+    filename="/etc/gitlab-runner/config.toml"
+    NEW_TEXT="$(cat $filename)"
+    NEWLINE=$'\n'
+    ANNOTATION_HEADER="    [runners.kubernetes.pod_annotations]"
+    NEW_TEXT="${NEW_TEXT}${NEWLINE}${ANNOTATION_HEADER}"
+    IFS=' '
+    for ANNOTATION in $ANNOTATIONS; do
+        KEY="$(echo -n $ANNOTATION | cut -d '=' -f 1)"
+        VALUE="$(echo -n $ANNOTATION | cut -d '=' -f 2)"
+        TEXT="\"$KEY\" = \"$VALUE\""
+        IFS=''
+        NEW_TEXT="${NEW_TEXT}${NEWLINE}      ${TEXT}"
+        IFS=' '
+    done
+    IFS=''
+    echo $NEW_TEXT > /etc/gitlab-runner/config.toml
+    IFS="$oIFS"
+}
+
+main() {
+    echo "[*] Starting"
+    checkGitlabConnection
+    gitlab-runner register --non-interactive --url="${GITLAB_HOST}" --registration-token="${REGISTRATION_TOKEN}" --executor="${EXECUTOR}" --description="${DESCRIPTION}" --tag-list="${TAG}"
+
+    if [ "${EXECUTOR}" == "kubernetes" ]; then
+        echo "[*] Adding namespace to config"
+        sed -i "s/namespace = \"\"/namespace = \"${NAMESPACE}\"/" /etc/gitlab-runner/config.toml
+        if [[ "$DNS_IP_LIST" != "" ]]; then 
+            echo "[*] Adding host aliases"
+            addHostsToConfig "$DNS_IP_LIST"
+        fi
+        if [[ "$POD_ANNOTATIONS" ]]; then
+            echo "[*] Adding pod annotations"
+            addAnnotationsToPod "$POD_ANNOTATIONS"
+        fi
     fi
-fi
+    echo "[*] Done!"
 
+    /usr/bin/dumb-init /entrypoint run --user=gitlab-runner --working-directory=/home/gitlab-runner
+}
 
-/usr/bin/dumb-init /entrypoint run --user=gitlab-runner --working-directory=/home/gitlab-runner
+main
